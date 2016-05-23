36e25ec085374d69958e28c68d7617ba5f10b890
Zhe Zhang <neomana@gmail.com>
2016-05-23 10:22:11 +0800

    wrap up final code for the day

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 /* global d3, WebSocket, XMLHttpRequest, fetch */
 
 // Defines variables
-const margin = {top: 50, right: 40, bottom: 50, left: 40}
+const margin = {top: 30, right: 40, bottom: 30, left: 40}
 const width = window.innerWidth - margin.left - margin.right
 const height = window.innerHeight - margin.top - margin.bottom
 
@@ -56,56 +56,58 @@ let svg = d3.select('body').append('svg')
   .append('g')
   .attr('class', 'content')
 
-d3.select('.btn-zoom-reset').on('click', () => {
-})
-
 let i = 0
 let duration = 750
 let root
 
 // Get the data in JSON format
-d3.json('data/testfile.json', (error, response) => {
-  if (error) throw error
+const getData = (url) => {
+  d3.json(url, (error, response) => {
+    if (error) throw error
 
-  // Count the numbers of app, image and containers
-  stats = response.reduce((stats, data, index, response) => {
-    stats.apps = response.length
-    stats.images += data.Layers.length
-    data.Layers.forEach((layers) => {
-      if (layers.Containerlist) {
-        stats.containers += layers.Containerlist.length
-      }
-    })
-    return stats
-  }, stats)
+    // Count the numbers of app, image and containers
+    stats = response.reduce((stats, data, index, response) => {
+      stats.apps = response.length
+      stats.images += data.Layers.length
+      data.Layers.forEach((layers) => {
+        if (layers.Containerlist) {
+          stats.containers += layers.Containerlist.length
+        }
+      })
+      return stats
+    }, stats)
 
-  // Update to display statistics
-  d3.select('.stats-apps .stats-value').text(stats.apps)
-  d3.select('.stats-images .stats-value').text(stats.images)
-  d3.select('.stats-containers .stats-value').text(stats.containers)
+    // Update to display statistics
+    d3.select('.stats-apps .stats-value').text(stats.apps)
+    d3.select('.stats-images .stats-value').text(stats.images)
+    d3.select('.stats-containers .stats-value').text(stats.containers)
 
-  // Wrap the data source array into an Object
-  let data = {
-    name: 'Node',
-    children: response
-  }
-
-  root = data
-  root.x0 = height / 2
-  root.y0 = 0
-
-  function collapse (d) {
-    if (d.children) {
-      d._children = d.children
-      d._children.forEach(collapse)
-      d.children = null
+    // Wrap the data source array into an Object
+    let data = {
+      name: 'Node',
+      children: response
     }
-  }
 
-  // root.children.forEach(normalize)
-  root.children.forEach(collapse)
-  update(root)
-})
+    root = data
+    root.x0 = height / 2
+    root.y0 = 0
+
+    function collapse (d) {
+      if (d.children) {
+        d._children = d.children
+        d._children.forEach(collapse)
+        d.children = null
+      }
+    }
+
+    // root.children.forEach(normalize)
+    root.children.forEach(collapse)
+    update(root)
+  })
+}
+
+// getData('data/online.json')
+getData('http://121.41.97.152:9998/crmonitor/project')
 
 d3.select(self.frameElement).style('height', height + 'px')
 
@@ -145,7 +147,6 @@ const update = (source) => {
   // Update the nodes…
   var node = svg.selectAll('g.node')
     .data(nodes, function (d) { return d.id || (d.id = ++i) })
-
   // Enter any new nodes at the parent's previous position.
   var nodeEnter = node.enter().append('g')
     .attr('class', 'node')
@@ -159,9 +160,33 @@ const update = (source) => {
     })
     .attr('transform', function (d) { return 'translate(' + source.y0 + ',' + source.x0 + ')'; })
     .on('click', click)
+    .on('mouseover', (d) => {
+      let cache = []
+      d3.select('#tooltip')
+        .style('left', (d.y + 50) + 'px')
+        .style('top', (d.x + 50) + 'px')
+        .select('#tooltip pre')
+        .text(JSON.stringify(d, (key, value) => {
+          if (typeof value === 'object' && value !== null) {
+            if (cache.indexOf(value) !== -1) {
+              // Circular reference found, discard key
+              return
+            }
+            // Store value in our collection
+            cache.push(value)
+          }
+          return value
+        }, 2))
 
-  // .append('circle')
-  // .attr('r', 1e-6)
+      cache = null
+      // Show the tooltip
+      d3.select('#tooltip').classed('hidden', false)
+    })
+    .on('mouseout', () => {
+      // Hide the tooltip
+      d3.select('#tooltip').classed('hidden', true)
+    })
+
   nodeEnter
     .filter(d => d.type === 'container')
     .append('polygon')
@@ -263,24 +288,8 @@ function click (d) {
     d.children = d._children
     d._children = null
   }
-  console.log(d)
-  // d.parent.children.pop()
-  // d.parent.children.push()
   update(d)
 }
-
-// function dragstarted (d) {
-//   d3.event.sourceEvent.stopPropagation()
-//   d3.select(this).classed('dragging', true)
-// }
-
-// function dragged (d) {
-//   d3.select(this).attr('cx', d.x = d3.event.x).attr('cy', d.y = d3.event.y)
-// }
-
-// function dragended (d) {
-//   d3.select(this).classed('dragging', false)
-// }
 
 const setup = () => {
   let ws = new WebSocket('ws://104.236.190.90:5000/conn')
@@ -304,12 +313,19 @@ const setup = () => {
   // Listen for new messages arriving at the client
   ws.onmessage = (evt) => {
     console.log('MESSAGE RECEIVED: ' + evt.data)
+    // console.log(JSON.parse(evt.data))
+    let response = JSON.parse(evt.data)
+    if (response.event === 'delete') {
+      destroyContainer(response)
+    } else if (response.event === 'set') {
+      createContainer(response)
+    }
   }
 }
 
 // The window.addEventListener triggers when the web page is loaded
 // then call the setup function window.addEventListener("load", setup, false)
-// window.addEventListener('load', setup, false)
+window.addEventListener('load', setup, false)
 
 // Upload compose.yml file
 
@@ -317,48 +333,96 @@ let form = document.getElementById('file-form')
 let fileSelect = document.getElementById('file-select')
 let uploadButton = document.getElementById('upload-button')
 
-form.onsubmit = function (event) {
-  event.preventDefault()
+uploadButton.addEventListener('click', (e) => {
+  e.preventDefault()
 
-  // Update button text.
-  uploadButton.innerHTML = 'Uploading...'
+  let input = document.querySelector('input[type="file"]')
 
-  let files = fileSelect.files
-  let formData = new FormData()
-  formData.append('file', fileSelect.files[0])
-  formData.append('Projectname', 'test_e')
+  let data = new FormData()
+  data.append('composefile', input.files[0])
+  data.append('projectname', 'projectd')
+  fetch('http://121.41.97.152:9998/crmonitor/register', {
+    method: 'POST',
+    body: data
+  })
+    .then(response => {
+      getData('http://121.41.97.152:9998/crmonitor/project')
+    })
+})
 
-  // // Loop through each of the selected files.
-  // for (var i = 0; i < files.length; i++) {
-  //   var file = files[i]
+// fetch('http://121.41.97.152:9998/crmonitor/project')
+//   .then(response => response.json())
+//   .then(json => {
+//     console.log('parsed json', json)
+//   })
+//   .catch(ex => {
+//     console.log('parsing failed', ex)
+//   })
 
-  //   // Check the file type.
-  //   if (!file.type.match('image.*')) {
-  //     continue
-  //   }
+// // http://121.41.97.152:9998/crmonitor/project
+// const fetchData = () => {
+// }
 
-  //   // Add the file to the request.
-  //   formData.append('photos[]', file, file.name)
-  // }
+let btnAnother = document.querySelector('.btn-another')
+let btnCreate = document.querySelector('.btn-create')
+// let btnScale = document.querySelector('.btn-scale')
+// let btnRefresh = document.querySelector('.btn-refresh')
 
-  let xhr = new XMLHttpRequest()
+btnAnother.addEventListener('click', () => {
+  getData('/data/testfile.json')
+})
 
-  // Set up a handler for when the request finishes.
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      // File(s) uploaded.
-      uploadButton.innerHTML = 'Upload'
-    } else {
-      console.log('An error occurred!')
+btnCreate.addEventListener('click', () => {
+  fetch('/data/create.json')
+    .then(response => response.json())
+    .then(json => {
+      root.children.push(json)
+    })
+  update(root)
+})
+
+// btnScale.addEventListener('click', () => {
+//   console.log('obj')
+//   console.log(nodes)
+//   let data = { Hostip: '10.211.55.15', container_id: '30595b1796ba0a24ef8901041ed9c4515e61a591c7485a5a548994dabfcbd31d', event: 'destroy', project_name: 'projectd', service_name: 'db', image_name: 'mysql'}
+//   console.log(root)
+//   root.children.forEach(project => {
+//     if (project.Projectname === data.project_name) {
+//       project.children.forEach(image => {
+//         if (image.Name === data.service_name) {
+//           image.children = image.children.filter(child => child.Id !== data.container_id)
+//         }
+//       })
+//     }
+//   })
+//   update(root)
+// })
+
+const destroyContainer = (data) => {
+  root.children.forEach(project => {
+    if (project.Projectname === data.project_name) {
+      project.children.forEach(image => {
+        if (image.Imagename === data.image_name) {
+          image.children = image.children.filter(child => child.Id !== data.container_id)
+        }
+      })
     }
-  }
-  xhr.send(formData)
+  })
+  update(root)
 }
 
-fetch('http://121.41.97.152:9998/crmonitor/project')
-  .then(response => response.json())
-  .then(json => {
-    console.log('parsed json', json)
+const createContainer = (data) => {
+  root.children.forEach(project => {
+    if (project.Projectname === data.project_name) {
+      project.children.forEach(image => {
+        if (image.Imagename === data.image_name) {
+          data.Hostip = '10.211.55.15'
+          data.Id = data.container_id
+          data.Status = 'Up 1 second ago'
+          image.children.push(data)
+        }
+      })
+    }
   })
-  .catch(ex => {
-    console.log('parsing failed', ex)})
+  update(root)
+}
